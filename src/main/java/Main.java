39e92c152a7a4c862567cc8215258b8572f79a0d
projectormato <k.keita811.anuy@gmail.com>
2020-01-05 13:51:36 +0900

    refactor: Args.javaを使用するMain.javaを実装

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        // for example.
+        // run with java main -p 40 -d Hello -l true
+        try {
+            Args arg = new Args("l, p# ,d*", args);
+            System.out.println("真偽値の引数: " + arg.getBoolean('l'));
+            System.out.println("整数の引数: " + arg.getInt('p'));
+            System.out.println("文字列の引数: " + arg.getString('d'));
+        } catch (ArgsException e) {
+            System.out.printf("引数エラー: %s\n", e.getErrorMessage());
+        }
     }
 }
